d5547b2c7c60b956817392a338f492903bd37260
michal-ci <michalcieciera1@gmail.com>
2023-05-24 14:02:26 +0200

    Fix scrypt scroll mobile

--- js/scroll.js
+++ js/scroll.js
@@ -1,76 +1,51 @@
-// const container = document.querySelector(".container");
-// let scrollLine = document.querySelector(".scroll");
-
-// container.addEventListener("wheel", (e) => {
-//   e.preventDefault();
-//   container.scrollLeft += e.deltaY;
-//   scrollLine.style.width = container.scrollLeft / 10 + "px";
-// });
-
-// const container = document.querySelector(".container");
-// let scrollLine = document.querySelector(".scroll");
-
-// container.addEventListener("wheel", (e) => {
-//   e.preventDefault();
-//   if (window.innerWidth < 750) {
-//     // Przewijanie w pionie, jeżeli szerokość okna jest mniejsza niż 750px
-//     container.scrollTop += e.deltaY;
-//   } else {
-//     // Przewijanie w poziomie, jeżeli szerokość okna jest większa lub równa 750px
-//     container.scrollLeft += e.deltaY;
-//     scrollLine.style.width = container.scrollLeft / 10 + "px";
-//   }
-// });
-
 const container = document.querySelector(".container");
 let scrollLine = document.querySelector(".scroll");
 
-let startTouchPosition = null;
+let isHorizontalScroll = window.innerWidth >= 750;
+let lastTouchY = 0;
+let lastTouchX = 0;
+
+window.addEventListener(
+  "resize",
+  function () {
+    isHorizontalScroll = window.innerWidth >= 750;
+  },
+  false
+);
+
+// Obsługa przewijania
+function scrollHandler(e) {
+  let deltaY = 0;
+  if (e.type === "wheel") {
+    deltaY = e.deltaY;
+  } else if (e.type === "touchmove") {
+    deltaY = lastTouchY - e.touches[0].clientY;
+    lastTouchY = e.touches[0].clientY;
+    lastTouchX = e.touches[0].clientX;
+  }
+  e.preventDefault();
+  if (isHorizontalScroll) {
+    // Przewijanie w poziomie
+    container.scrollLeft += deltaY;
+    scrollLine.style.width = container.scrollLeft / 10 + "px";
+  } else {
+    // Przewijanie w pionie
+    window.scrollBy(0, deltaY);
+  }
+}
 
 // Obsługa przewijania za pomocą kółka myszy
-container.addEventListener("wheel", (e) => {
-  e.preventDefault();
-  if (window.innerWidth < 750) {
-    // Przewijanie w pionie, jeżeli szerokość okna jest mniejsza niż 750px
-    container.scrollTop += e.deltaY;
-  } else {
-    // Przewijanie w poziomie, jeżeli szerokość okna jest większa lub równa 750px
-    container.scrollLeft += e.deltaY;
-    scrollLine.style.width = container.scrollLeft / 10 + "px";
-  }
-});
+container.addEventListener("wheel", scrollHandler);
 
 // Obsługa przewijania za pomocą gestów dotykowych
 container.addEventListener(
   "touchstart",
   function (e) {
     // Zapisz pozycję początkową dotknięcia
-    startTouchPosition = e.touches[0].pageY;
+    lastTouchY = e.touches[0].clientY;
+    lastTouchX = e.touches[0].clientX;
   },
   false
 );
 
-container.addEventListener(
-  "touchmove",
-  function (e) {
-    // Zapobiegaj domyślnemu przewijaniu przeglądarki
-    e.preventDefault();
-
-    let touchPosition = e.touches[0].pageY;
-    let scrollAmount = startTouchPosition - touchPosition;
-
-    if (window.innerWidth < 750) {
-      // Przewijanie w pionie, jeżeli szerokość okna jest mniejsza niż 750px
-      container.scrollTop += scrollAmount;
-    } else {
-      // Przewijanie w poziomie, jeżeli szerokość okna jest większa lub równa 750px
-      container.scrollLeft += scrollAmount;
-      scrollLine.style.width = container.scrollLeft / 10 + "px";
-    }
-
-    // Aktualizuj początkową pozycję dotknięcia dla kolejnego przewinięcia
-    startTouchPosition = touchPosition;
-  },
-  false
-);
-console.log("test scryptu ");
+container.addEventListener("touchmove", scrollHandler);
